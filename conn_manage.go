98d5df6af133c84f6657d7ce8c89893a6eb03904
gsxhnd <gsxhnd@gmail.com>
2021-01-25 18:49:49 +0800

    update

--- conn_manage.go
+++ conn_manage.go
@@ -3,6 +3,11 @@ package tower
 import "sync"
 
 type ConnManager interface {
+	Add(conn Connectioner)                 //添加链接
+	Remove(conn Connectioner)              //删除连接
+	Get(connID uint) (Connectioner, error) //利用ConnID获取链接
+	Len() int                              //获取当前连接
+	ClearConn()                            //删除并停止所有链接
 }
 
 type ConnManage struct {
@@ -14,4 +19,24 @@ func NewConnManage() *ConnManage {
 	return &ConnManage{
 		connections: make(map[uint]Connectioner),
 	}
+}
+
+func (c *ConnManage) Add(conn Connectioner) {
+	panic("implement me")
+}
+
+func (c *ConnManage) Remove(conn Connectioner) {
+	panic("implement me")
+}
+
+func (c *ConnManage) Get(connID uint) (Connectioner, error) {
+	panic("implement me")
+}
+
+func (c *ConnManage) Len() int {
+	panic("implement me")
+}
+
+func (c *ConnManage) ClearConn() {
+	panic("implement me")
 }
