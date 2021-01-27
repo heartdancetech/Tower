80fcd246e5bce624abe7aaeffcaddc93a94fc0c4
gsxhnd <gsxhnd@gmail.com>
2021-01-27 14:13:27 +0800

    test: log

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -2,20 +2,40 @@ package logger
 
 import (
 	"github.com/stretchr/testify/assert"
+	"log"
+	"os"
 	"testing"
 )
+
+var (
+	testLogger = NewLogger(log.New(os.Stdout, "[Tower] ", log.Ldate|log.Ltime|log.LUTC), Debug)
+)
+
+func TestNewLogger(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{"test"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var _ Logger = new(logging)
+		})
+	}
+}
 
 func Test_logging_LogMode(t *testing.T) {
 	tests := []struct {
 		name string
-		want Logger
+		mode LogLevel
 	}{
-		{},
+		{"test_debug", Debug},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Nil(t, nil)
-			assert.Nil(t, nil)
+			var testLogging = NewLogger(log.New(os.Stdout, "[Tower] ", log.Ldate|log.Ltime|log.LUTC), tt.mode)
+			testLogging.LogMode(Debug)
+			assert.NotNil(t, testLogging)
 		})
 	}
 }
