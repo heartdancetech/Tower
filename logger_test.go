6f1a45e4b8e0e4c9d2294fbc35605deca0c69e44
gsxhnd <gsxhnd@gmail.com>
2021-02-24 11:49:06 +0800

    test: fix test

--- logger_test.go
+++ logger_test.go
@@ -19,7 +19,7 @@ func TestNewLogger(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			var _ logger = new(logging)
+			var _ Logger = new(logging)
 		})
 	}
 }
