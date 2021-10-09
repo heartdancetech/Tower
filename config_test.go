db468e96e0f74c1425d8d8d54115d25fd538f8bf
gsxhnd <gsxhnd@gmail.com>
2021-10-09 14:02:05 +0800

    test: fix conf test

--- config_test.go
+++ config_test.go
@@ -39,15 +39,10 @@ func TestConfig_check(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			c := Config{}
 			c.check()
-			assert.NotEqual(t, "", c.IP)
 			assert.Equal(t, "0.0.0.0", c.IP)
-			assert.NotEqual(t, 0, c.Port)
 			assert.Equal(t, 8999, c.Port)
-			assert.NotEqual(t, 0, c.MaxConn)
 			assert.Equal(t, 1024, c.MaxConn)
-			assert.NotEqual(t, 0, c.MaxPacketSize)
-			assert.Equal(t, uint32(4096), c.MaxPacketSize)
-			assert.NotEqual(t, 0, c.MaxMsgChanLen)
+			assert.Equal(t, 0, c.MaxPacketSize)
 			assert.Equal(t, uint32(1024), c.MaxMsgChanLen)
 		})
 	}
